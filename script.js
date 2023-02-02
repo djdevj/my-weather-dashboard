d4e671760b89921610ed6bd70beb6e7d8b43417f
Devlin Jenkins <djdevj@gmail.com>
2023-02-01 17:51:18 -0800

    updated code

--- script.js
+++ script.js
@@ -1,38 +1,94 @@
 // Get location for the forcast
 // Start call back functions:
 // write function for getting the location "getPosition"
-var gotPosition = function(pos) {
-    var lat = pos.coords.latitude; //define rule for latitude
-    var long = pos.coords.longitude; //define rule for longitude
-    //console.log(lat, long)
-    getForcast(lat, long) //call position to get forcast
+let gotPosition = function(pos) {
+    let lat = pos.coords.latitude; //define rule for latitude
+    let long = pos.coords.longitude; //define rule for longitude
+    
+    getForecast(lat, long); //call position to get forcast
 }
-// write function for getting the forcast "getForcast"
-var getForcast = function(lat, long) {
-    var url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=7728e4dc7084f1f64c5456d0ab073c58"; //weather API defined string
+// write function for getting the forcast "getForecast"
+let getForecast = function(lat, long) {
+    let url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=7728e4dc7084f1f64c5456d0ab073c58"; //weather API defined string
     getWeatherText(url);
     
 }
 
 // write function for syncing the weather 
 async function getWeatherText(url) {
-    var weatherObject = await fetch(url); //making it wait to fetch data
-    var weatherText = await weatherObject.text(); //making the info readable
+    let weatherObject = await fetch(url); //making it wait to fetch data
+    let weatherText = await weatherObject.text(); //making the info readable
+    console.log(weatherObject);
+    console.log(weatherText);
     parseWeather(weatherText);
 }
 
 // Create parse weather function (for daily forcast)
-var parseWeather = function(weatherText) {
-    var weatherJson = JSON.parse(weatherText);
-    var dailyForcast = weatherJson.daily;
-    //console.log(weatherJson)
-    for (x = 0; x < dailyForcast.length; x++) {
-        var day = dailyForcast[x];
-        var today = new Date().getDay() + x;
+let parseWeather = function(weatherText) {
+    let weatherJSON = JSON.parse(weatherText);
+    console.log(weatherJSON);
+    let dailyForecast = weatherJSON.daily;
+    console.log(dailyForecast)
+    
+    for (x = 0; x < dailyForecast.length; x++) {
+        let day = dailyForecast[x];
+        let today = new Date().getDay() + x;
         if (today > 6) {
             today = today - 7;
         }
+        let dayOfWeek = getDayOfWeek(today);
+        let description = day.weather[0].description;
+        let icon = day.weather[0].icon;
+        let highTemp = kToF(day.temp.max);
+        let lowTemp = kToF(day.temp.min);
+        let humidity = day.humidity;
+        displayWeatherDay(dayOfWeek, description, icon, highTemp, lowTemp, humidity);
     }
+}
+
+// Setting what's displayed in each card
+let displayWeatherDay = function(dayOfWeek, description, icon, highTemp, lowTemp, humidity){
+    let out = "<div class='weatherDay'><img src='http://openweathermap.org/img/wn/" + icon + ".png'/>";
+    out += "<h2>" + dayOfWeek + "</h2>";
+    out += "<h3>" + description + "</h3>";
+    out += "<p>" + icon + "</p>";
+    out += "<p>" + highTemp + "</p>";
+    out += "<p>" + lowTemp + "</p>";
+    out += "<p>" + humidity + "</p>";
+    document.getElementById("forcast").innerHTML += out;
+}
+
+// Defining days of the week
+let getDayOfWeek = function(dayNum){
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    return (weekday[dayNum]);
+}
+
+// Weather calculations
+let kToF = function(kelvinTemp){
+    const celsius = kelvinTemp -273;
+    const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+    return fahrenheit 
+}
+
+let timestampToTime = function(timeStamp){
+    let date = new Date(timeStamp * 1000);
+    let hours = date.getHours();
+    let minutes = "";
+    if (date.getMinutes() < 10){
+        minutes = "0" + date.getMinutes();
+    } else {
+        minutes = date.getMinutes();
+    }
+    return hours + ":" + minutes;
 }
 
 navigator.geolocation.getCurrentPosition(gotPosition);
